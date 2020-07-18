47b263bbc3c889479eeefc003c548d7e090de155
Seikichi KONDO <seikichi@kmc.gr.jp>
2020-07-18 13:51:51 +0900

    Enhance transpiler

--- app/src/galaxy_transpiler.rs
+++ app/src/galaxy_transpiler.rs
@@ -5,6 +5,12 @@ use galaxy_interpreter::{load, Function, Statement};
 fn parse_cells(cells: &[Function]) -> (String, &[Function]) {
     match cells[0] {
         Function::Number(n) => (n.to_string(), &cells[1..]),
+        Function::Neg => ("_neg".to_string(), &cells[1..]),
+        Function::Ap => {
+            let (fun, rest1) = parse_cells(&cells[1..]);
+            let (arg, rest2) = parse_cells(&rest1);
+            (format!("((force {}) {})", fun, arg), rest2)
+        }
         _ => (String::from(""), &[]),
     }
 }
@@ -27,7 +33,13 @@ fn main() {
 
 #[test]
 fn test_transpile_statement() {
-    let cases = vec![(vec![Function::Number(42)], "42")];
+    let cases = vec![
+        (vec![Function::Number(42)], "42"),
+        (
+            vec![Function::Ap, Function::Neg, Function::Number(42)],
+            "((force _neg) 42)",
+        ),
+    ];
 
     for (cells, expected) in cases {
         let actual = transpile_statement(&Statement { id: 0, cells });
