fab1333783599f947752fd6a741e3357a4c5676b
Kohei NAGIRA <qwerty@kmc.gr.jp>
2020-07-18 20:47:28 +0900

    try to send

--- app/main.rs
+++ app/main.rs
@@ -16,7 +16,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let req = Request::builder()
         .method(Method::POST)
         .uri(server_url)
-        .body(Body::from(format!("1101000")))?;
+        .body(Body::from(format!("ap send 0")))?;
 
     match client.request(req).await {
         Ok(mut res) => match res.status() {
