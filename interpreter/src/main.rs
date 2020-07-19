2eb6d623b13e680824c28fa25bdfc0f90ae86bc9
Yusuke Nojima <nojima@ynojima.com>
2020-07-19 19:12:02 +0900

    Take state and vector as command line arguments

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,5 +1,6 @@
-use core::{AstNode, GalaxyEvaluator};
+use core::{AstNode, GalaxyEvaluator, demodulate};
 use std::thread;
+use std::env;
 
 #[allow(dead_code)]
 fn main() {
@@ -8,8 +9,22 @@ fn main() {
         .name("interpreter".to_owned())
         .stack_size(stack_size)
         .spawn(move || {
+            let args: Vec<String> = env::args().collect();
+
+            let state = if args.len() >= 2 {
+                demodulate(&args[1])
+            } else {
+                AstNode::make_nil()
+            };
+
+            let vector = if args.len() >= 3 {
+                demodulate(&args[2])
+            } else {
+                AstNode::make_vector(0, 0)
+            };
+
             let mut evaluator = GalaxyEvaluator::new();
-            let node = evaluator.evaluate(AstNode::make_nil(), AstNode::make_vector(0, 0));
+            let node = evaluator.evaluate(state, vector);
             println!("{}", node);
         })
         .unwrap();
