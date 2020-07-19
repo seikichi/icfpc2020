6852a5b85e7dd6303b20e8bdfab8eb765f15c357
Yusuke Nojima <nojima@ynojima.com>
2020-07-19 18:30:09 +0900

    Improve request log

--- ships/src/main.rs
+++ ships/src/main.rs
@@ -64,14 +64,13 @@ impl ProxyClient {
         args: Rc<AstNode>,
         purpose: &str /* for logging */
     ) -> Result<Rc<AstNode>, Error> {
-        let encoded_args = modulate(args);
-
         let param = self.api_key.as_ref()
             .map_or_else(|| "".to_owned(), |k| "?apiKey=".to_owned() + &k);
         let url = self.server_url.clone() + "/aliens/send" + &param;
 
-        info!("Request({}): url={}, body={}", purpose, url, encoded_args);
+        info!("Request({}): url={}, body={}", purpose, url, args);
 
+        let encoded_args = modulate(args);
         let client = reqwest::blocking::Client::new();
         let resp = client.post(&url).body(encoded_args.to_owned()).send()?;
 
