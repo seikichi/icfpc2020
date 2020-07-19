85d18ea2f1b1881c5a67144ea523ac4348faa846
Yusuke Nojima <nojima@ynojima.com>
2020-07-19 13:09:43 +0900

    Use reqwest

--- app/main.rs
+++ app/main.rs
@@ -1,7 +1,52 @@
+// extern crate failure;
+extern crate reqwest;
+
 use http_body::Body as _;
 use hyper::{Body, Client, Method, Request, StatusCode};
 use std::env;
 use std::process;
+// use failure::Error;
+
+/*
+pub struct ProxyClient {
+    server_url: String,
+    player_key: String,
+}
+
+impl ProxyClient {
+    pub fn new(server_url: &str, player_key: &str) -> Self {
+        Self {
+            server_url: server_url.to_owned(),
+            player_key: player_key.to_owned(),
+        }
+    }
+
+    fn send(self) -> Result<(), Error> {
+        let url = self.server_url + "/alians/send";
+        let req = Request::builder()
+            .method(Method::POST)
+            .uri(url)
+            .body(Body::from(format!("{}", self.player_key)))?;
+
+        let client = Client::new();
+        let res = client.request(req).await?;
+
+        match res.status() {
+            StatusCode::Ok => {
+                
+                while let Some(chunk) = res.body_mut().data().await {
+                    match chunk {
+                        Ok(content) => println!("{:?}", content),
+                        Err(why) => println!("error reading body: {:?}", why),
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+*/
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-/solution/target/release/app "$@" || echo "run error code: $?"
+REPO_TOP="$(cd $(dirname "$0"); pwd)"
+"$REPO_TOP/target/release/app" "$@" || echo "run error code: $?"
